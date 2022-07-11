c9ef85b0e6f51bc941b7970ab05443ab4b6f7bd3
camillovl <camillovl10@gmail.com>
2022-07-11 12:35:22 -0300

    Mudancas
    Implementacao de classes

--- 2DisplaySurface.py
+++ 2DisplaySurface.py
@@ -1,14 +1,133 @@
+from turtle import width
 import pygame
 from sys import exit
-from random import randint
+import random
+from random import randint,choice
 
-#creating a score mode
-def display_score():
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        #Walking
+        player_surface1 = pygame.image.load('WalkSoldier\Soldado (1).png').convert_alpha()
+        player_surface2 = pygame.image.load('WalkSoldier\Soldado (2).png').convert_alpha()
+        player_surface3 = pygame.image.load('WalkSoldier\Soldado (3).png').convert_alpha()
+        player_surface4 = pygame.image.load('WalkSoldier\Soldado (4).png').convert_alpha()
+
+        self.player_walk = [player_surface1,player_surface2,player_surface3,player_surface4]
+        self.player_index = 0
+        #self.player_surface = player_walk[player_index]
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(bottomright = (100,550))
+        self.gravity = 0
+
+        #Jumping
+        player_jump1 = pygame.image.load('jumpSoldier\Tile050.png').convert_alpha
+        player_jump2 = pygame.image.load('jumpSoldier\Tile051.png').convert_alpha
+        player_jump3 = pygame.image.load('jumpSoldier\Tile052.png').convert_alpha
+        player_jump4 = pygame.image.load('jumpSoldier\Tile053.png').convert_alpha
+
+        self.player_jump = [player_jump1,player_jump2,player_jump3,player_jump4]
+        self.player_index_jump = 0
+        #self.player_surface_jump = player_jump[player_index_jump]
+        
+        self.image_jump = self.player_jump[self.player_index_jump]
+    
+    
+    def player_animation(self):
+
+        if self.rect.bottom < 550:
+            self.player_index_jump += 0.1
+        if self.player_index_jump >= len(self.player_jump):
+            self.player_index_jump = 0
+            self.player_image_jump = self.player_jump[int(self.player_index_jump)]
+
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 550:
+            self.gravity = -17
+
+
+    
+    def apply_gravity(self):
+        self.gravity +=1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 550:
+            self.rect.bottom = 550
+    
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.player_animation()
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self,type):
+        super().__init__()
+
+        if type == 'stirge':
+            stirge_frame1 = pygame.image.load('1.png').convert_alpha()
+            stirge_frame1 = pygame.transform.scale(stirge_frame1,(50,50))
+            stirge_frame2 = pygame.image.load('2.png').convert_alpha()      
+            stirge_frame2 = pygame.transform.scale(stirge_frame2,(50,50))
+
+            #stirge_index = 0
+            self.frames = [stirge_frame1, stirge_frame2]
+            #stirge_surface = stirge_frames[stirge_index]
+            y_pos = 515
+        else:
+            snake_frame1 = pygame.image.load('Snake\Cobra 1.png').convert_alpha()
+            snake_frame1 = pygame.transform.scale(snake_frame1,(40,40))
+
+            snake_frame2 = pygame.image.load('Snake\Cobra 2.png').convert_alpha()
+            snake_frame2 = pygame.transform.scale(snake_frame2,(40,40))
+
+            snake_frame3 = pygame.image.load('Snake\Cobra 3.png').convert_alpha()
+            snake_frame3 = pygame.transform.scale(snake_frame3,(40,40))
+
+            snake_frame4 = pygame.image.load('Snake\Cobra 4.png').convert_alpha()
+            snake_frame4 = pygame.transform.scale(snake_frame4,(40,40))
+
+            #snake_index = 0
+            self.frames = [snake_frame1, snake_frame2, snake_frame3, snake_frame4]
+            #snake_surface = snake_frames[snake_index]
+            y_pos = 550
+
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (random.randint(900,1100),y_pos))
+    
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >=len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def update(self):
+        self.animation_state
+        self.rect.x -= 6
+        self.destroy()
+
+    def destroy(self):
+        if self.rect.x <= -100:
+            self.kill()
+
+#creating a timer
+def display_timer():
+
     current_time = int(pygame.time.get_ticks() /100) - start_time
-    score_surface = test_font.render(f'{current_time}',False,(64,64,64)) #curren_time NEED TO BE AN INT
-    score_rect = score_surface.get_rect(topleft = (425,50))
-    screen.blit(score_surface,score_rect)
-    print(current_time)
+    timer_surface = test_font.render(f'{current_time}',False,(64,64,64)) #curren_time NEED TO BE AN INT
+    timer_rect = timer_surface.get_rect(topleft = (425,50))
+    screen.blit(timer_surface,timer_rect)
+    #print(current_time)
 
 def obstacle_movement(obstacle_list):
     if obstacle_list:
@@ -33,18 +152,33 @@ def obstacle_movement(obstacle_list):
         #         aux.append(obstacle)
 
         return obstacle_list
-    else: return []
+    else:
+         return []
 
 
-def collisions(player,obstacles):
-
-    if obstacles:
-        for obstacles_rect in obstacles:
-            if player.colliderect(obstacles_rect): return game_active == False
-    return game_active == True
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            if player_rect.x >= obstacle_rect:
+                score_value += 1
 
 
-def player_animation():
+# def collisions(player,obstacles):
+
+#     if obstacles:
+#         for obstacles_rect in obstacles:
+#             if player.colliderect(obstacles_rect): return game_active == False
+#     return game_active == True
+
+def collision_sprite(): 
+    if pygame.sprite.spritecollide(player.sprite,obstacle_group,False):#1sprite, 2group, boolean:checks if the sprite collides with the group will be destroyed or noit
+    #this entire statment returns a list if doesnt collide with anything is gonna return an empity list
+        obstacle_group.empity()
+        return False
+    else:
+        return True
+
+
+#def player_animation():
     global player_surface, player_index, player_surface_jump, player_index_jump
 
     if player_rect.bottom < 550:
@@ -64,12 +198,21 @@ def player_animation():
 pygame.init()
 
 #Creating your screen and important variables
-screen = pygame.display.set_mode((800,600)) #widht,height #(()) its a tuple
+width = 800
+height = 600
+screen = pygame.display.set_mode((width,height)) #widht,height #(()) its a tuple
 pygame.display.set_caption('MapleStory Wannabe') #setting the name of the game
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('Pixeltype.ttf',30) #font type and font size
 game_active = False
-#start_time = 0
+start_time = 0
+
+#GROUPS
+player = pygame.sprite.GroupSingle() #this is a group single
+player.add(Player()) #this is a sprite
+
+obstacle_group = pygame.sprite.Group()
+
 
 #background_surface = pygame.image.load('backgrounds.png').convert_alpha() #converting an image to alpha facilitates pygame to read the code and make it lighter
 
@@ -78,6 +221,7 @@ title1_surface = pygame.image.load('Tiles\Dois.png').convert_alpha()
 title2_surface = pygame.image.load('Tiles\Dois.png').convert_alpha()
 title3_surface = pygame.image.load('Tiles\Dois.png').convert_alpha()
 title4_surface = pygame.image.load('Tiles\Dois.png').convert_alpha()
+
 title5_surface = pygame.image.load('Tiles\Dois.png').convert_alpha()
 title6_surface = pygame.image.load('Tiles\Dois.png').convert_alpha()
 title7_surface = pygame.image.load('Tiles\Dois.png').convert_alpha()
@@ -85,9 +229,13 @@ title8_surface = pygame.image.load('Tiles\Dois.png').convert_alpha()
 
 game_over = pygame.image.load('gameover.png').convert_alpha()
 
-#score 
-score_surface = test_font.render('Timer:', False, 'Black') #text you want to display, anti anliasing(False or True) , color
-score_rect = score_surface.get_rect(topleft = (320,47 ))
+#score
+score_surface = test_font.render('Score: ',False, 'Black')
+score_rect = score_surface.get_rect(topleft = (600 ,40))
+
+#timer
+timer_surface = test_font.render('Timer: ', False, 'Black') #text you want to display, anti anliasing(False or True) , color
+timer_rect = timer_surface.get_rect(topleft = (320,47 ))
 
 #obstacles
 snake_frame1 = pygame.image.load('Snake\Cobra 1.png').convert_alpha()
@@ -204,11 +352,12 @@ while True:
 
         if game_active == True:
             if event.type == obstacle_timer:
-                if randint(0,2):#random statment that triggers true or false (0 or 1)
-                    obstacle_rect_list.append(snake_frames[snake_index].get_rect(bottomright = (randint(1100,1200),550)))
-                else:
-                    obstacle_rect_list.append(stirge_frames[stirge_index].get_rect(bottomright = (randint(1100,1200),515)))
-                    #print('test')
+                obstacle_group.add(Obstacle(choice(['stirge','snail','snail'])))
+                # if randint(0,2):#random statment that triggers true or false (0 or 1)
+                #     obstacle_rect_list.append(snake_frames[snake_index].get_rect(bottomright = (randint(1100,1200),550)))
+                # else:
+                #     obstacle_rect_list.append(stirge_frames[stirge_index].get_rect(bottomright = (randint(1100,1200),515)))
+                #     print('test')
     
             if event.type == snake_animation_timer and game_active == True:
                 snake_index = (snake_index + 1) % 4
@@ -260,10 +409,15 @@ while True:
         screen.blit(title6_surface,(672,550))  #blit is used when you want to put a surface in another surface    
         screen.blit(title7_surface,(800,550))  #blit is used when you want to put a surface in another surface    
         screen.blit(title8_surface,(928,550))  #blit is used when you want to put a surface in another surface    
-    #Score  
+    
+    #score
+
+
+
+    #timer
         #pygame.draw.rect(screen,'Black',score_rect)
-        screen.blit(score_surface,score_rect)
-        display_score()
+        screen.blit(timer_surface,timer_rect)
+        display_timer()
         
     #Snake   
         # snake_rect.x -= 3
@@ -273,25 +427,33 @@ while True:
         # screen.blit(snake_surface,(snake_rect))
 
     #player
-        player_gravity += 1
-        player_rect.y += player_gravity
-        
+        # player_gravity += 1
+        # player_rect.y += player_gravity
+
+        player.draw(screen)
+        player.update()
+
+
+        obstacle_group.draw(screen)
+        obstacle_group.update()
+
+    #Colision 
+        game_active = collision_sprite()
         #simulating the colision with the floor
         if player_rect.bottom >= 550:
             player_rect.bottom = 550
 
-        player_animation()
+       #player_animation()
 
         
         #pygame.draw.rect(screen,'Blue',player_rect)
         screen.blit(player_surface,(player_rect))   
 
     #Obstacle movement
-        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+        #obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
     #collision
-
-        game_active = collisions(player_rect,obstacle_rect_list) #collisions can return false or true, if the player hits an obstacle it will return false ending the
+        #game_active = collisions(player_rect,obstacle_rect_list) #collisions can return false or true, if the player hits an obstacle it will return false ending the
 
         #player_rect.colliderect(snake_rect)
         #print(player_rect.colliderect(snake_rect)) #the moment rectangle colides it becames true
